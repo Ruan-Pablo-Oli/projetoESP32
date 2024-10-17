4fdca1aafbe98c0a5d5a541311b8256155c49d83
Ruan <ruanpablooliveira20@gmail.com>
2024-10-17 07:47:10 -0300

    add PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 const api = require('./rotas/')
 app.use('/api',api)
 
-const PORT = 3080
+const PORT = process.env.PORT
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('frontend/build'))
